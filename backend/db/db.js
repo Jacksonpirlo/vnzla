487b481a50c7c3f5fc43fe96b045bfb9684dfe0c
Jackson Florez <141880433+Jacksonpirlo@users.noreply.github.com>
2025-08-11 15:15:53 -0500

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,8 +4,8 @@ const { Pool } = pkg;
 // Configuración de PostgreSQL con datos reales
 const db = new Pool({
   host: 'aws-0-us-east-2.pooler.supabase.com',
-  user: 'postgres.wsuvuakmzpezlmfpqded',
-  password: '1234', // remplaza con tu contraseña de Supabase
+  user: 'postgres.3lw3b0m103st4pr3nd10
+  password: 'password', // remplaza con tu contraseña de Supabase
   database: 'postgres',
   port: 6543,
   ssl: { rejectUnauthorized: false } // Supabase requiere SSL
